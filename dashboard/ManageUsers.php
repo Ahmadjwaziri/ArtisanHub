79714cbe41d0efe7276a79f74f05c433cf803247
Yaroline <joshyaroline@gmail.com>
2022-09-15 13:25:22 +0100

    init

---
+++ dashboard/ManageUsers.php
@@ -0,0 +1,63 @@
+<?php include ('header.php'); ?>
+
+
+<link media="all" type="text/css" rel="stylesheet" href="assets/libs/data-table/datatables.min.css">
+<br><br>
+
+<div class="container-fluid pt-4 px-4">
+                <div class="row g-4">
+<div class="col-12">
+                        <div class="bg-secondary rounded h-100 p-4">
+                        <div class="panel-heading">
+                            
+                            <a class="btn btn-primary btn-xs" data-toggle="modal" href="#myModal" data-target="#myModal"><i class="fa fa-edit"></i> Add New Admin</a>
+                            <br><br>
+                            <h6 class="mb-4">Users Tables</h6>
+                        </div>
+                        <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names.." title="Type in a name" style="float:right;">
+                            <div id="myTable" class="table data-table table-hover table-ultra-responsive">
+                                <table class="table data-table table-hover table-ultra-responsive">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">S/N</th>
+                                            <th scope="col">Full Name</th>
+                                            <th scope="col">Phone</th>
+                                            <th scope="col">Email</th>
+                                            <th scope="col">Role</th>
+                                            <th scope="col">Status</th>
+                                            <th scope="col">Action</th>
+
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th scope="row">1</th>
+                                            <td>Ahmad Jafar Waziri</td>
+                                            <td>08097754572</td>
+                                            <td>ahmedjafarwaziri@email.com</td>
+                                            <td>Admin</td>
+                                            <td>Active</td>
+                                            <td>
+                                            <a href="#"><button type="button" class="btn btn-danger m-2">Danger</button></a>
+                                            <a href="#"><button type="button" class="btn btn-success m-2">Enable</button></a>
+                                            <a href="#"><button type="button" class="btn btn-danger m-2">Disable</button></a>
+                                            </td>
+                                        </tr>
+                                        
+                                        
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+             </div>
+
+
+
+
+           
+            
+           <?php include ('footer.php'); ?>
+
+    
---
+++ dashboard/footer.php
@@ -0,0 +1,69 @@
+
+
+
+<!-- Footer Start -->
+
+<!-- Footer End -->
+</div>
+<!-- Content End -->
+
+
+<!-- Back to Top -->
+<a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+</div>
+  <!-- JavaScript Libraries -->
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="lib/chart/chart.min.js"></script>
+<script src="lib/easing/easing.min.js"></script>
+<script src="lib/waypoints/waypoints.min.js"></script>
+<script src="lib/owlcarousel/owl.carousel.min.js"></script>
+<script src="lib/tempusdominus/js/moment.min.js"></script>
+<script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
+<script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+<script src="assets/libs/jquery-1.10.2.min.js"></script>
+
+<script src="assets/libs/jquery.slimscroll.min.js"></script>
+
+<script src="assets/libs/smoothscroll.min.js"></script>
+
+<script src="assets/libs/bootstrap/js/bootstrap.min.js"></script>
+
+<script src="assets/libs/bootstrap-toggle/js/bootstrap-toggle.min.js"></script>
+
+<script src="assets/libs/alertify/js/alertify.js"></script>
+
+<script src="assets/libs/bootstrap-select/js/bootstrap-select.min.js"></script>
+
+<script src="assets/js/scripts.js"></script>
+
+<!-- Template Javascript -->
+<script src="js/main.js"></script>
+
+<script>
+function myFunction() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+</script>
+
+</body>
+
+</html>
