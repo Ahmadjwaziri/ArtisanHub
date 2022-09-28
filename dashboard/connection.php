b439def120ec95f2ec7d929f2e92ea34cd579a3a
Yaroline <joshyaroline@gmail.com>
2022-09-28 12:10:08 +0100

    Register users

---
+++ dashboard/connection.php
@@ -0,0 +1,10 @@
+
+<?php
+
+$connect = mysqli_connect('127.0.0.1:3306','root','','artisanhub') or die("error connection");
+
+
+
+
+
+?>
---
+++ dashboard/register.php
@@ -0,0 +1,56 @@
+<?php 
+include ('connection.php');
+
+$fullname = $_POST['fullname'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$address = $_POST['address'];
+$password = $_POST['password'];
+$cpassword = $_POST['cpassword'];
+
+
+
+$check = mysqli_query($connect,"SELECT * FROM users where user_email='$email'");
+
+if($check->num_rows>0){
+echo '
+<script>
+
+alert("Email already exist");
+window.location=("signup.html");
+</script>
+';
+
+
+}else{
+if($password==$cpassword){
+$iid = "user";
+$user_id = "user" .rand(0000,99999)."";
+$pass = md5($password);
+$avatar="avatar.jpg";
+$insert = mysqli_query($connect, "INSERT INTO users(user_id,user_password,user_email,user_phone,user_fulname,user_address,user_photo,user_status) VALUES('$user_id','$pass', '$email', '$phone', '$fullname', '$address','$avatar','1') ");    
+echo '
+<script>
+    alert("Registration Successfully");
+    window.location =("index.html");
+    </script>
+';
+}else{
+    echo '
+    <script>
+ alert("Password and Confirm Password does not match");
+  window.location =("signup.html");
+  </script>
+  
+  ';
+}
+
+}
+
+
+
+
+
+
+
+?>
